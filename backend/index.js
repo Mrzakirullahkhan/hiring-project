edfbdaa26d84c91deb3cd0cd38b2020a8898ec5c
Mr zakir ullah <mrzak802@gmail.com>
2024-12-14 11:51:13 -0800

    comment

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express"
 import cookieParser from "cookie-parser";
 import cors from "cors"
-
+import dotenv from "dotenv"
+import connectDB from "./utils/db.js";
+dotenv.config({})
 const app = express();
 
 // middleware
@@ -12,15 +14,11 @@ const corsOptions ={
     origin:"http//localhost:5173",
     Credentials:true
 }
-app.get("/home",(req,res)=>{
-        return res.status(200).json({
-            message:"i am coming from backing",
-            success:true
-        })
-})
+
 app.use(cors(corsOptions))
 
-const PORT = 3000;
+const PORT = process.env.PORT|| 3000;
+connectDB()
 app.listen(PORT,()=>{
     console.log(`server running on PORT ${PORT}`)
 })
