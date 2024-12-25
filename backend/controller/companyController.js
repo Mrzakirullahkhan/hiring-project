f632cd34322314a1c7b2d99961a52708b9f59666
Mr zakir ullah <mrzak802@gmail.com>
2024-12-25 03:21:25 -0800

    comment

--- backend/controller/companyController.js
+++ backend/controller/companyController.js
@@ -31,3 +31,39 @@ const registerCompany = async (req,res)=>{
         console.log(error)
     }
 }
+
+// ab jo company banai hai mene isko get bhi krna hai mere uskje liye bana rha hu ab 
+export const getCompany = async (req,res)=> {
+      try {
+        const userId = req.id; //logged in user id
+        const companies = await Company.find({userId});
+        if(!companies){
+            return res.status(404).json({
+                message:"companies not found !",
+                status:false
+            })
+        }
+      } catch (error) {
+        console.log(error)
+      }
+}
+
+// get company by Id
+export const getCompanyById = async (req,res)=>{
+       try {
+        const companyId = req.params.id;
+        const company = await Company.findById(companyId);
+        if(!companyId){
+           return res.status.json({
+            message:"No company Found !",
+            status:false
+           })
+        }
+        return res.status(200).json({
+            company,
+            status:true
+        })
+       } catch (error) {
+        
+       }
+}
