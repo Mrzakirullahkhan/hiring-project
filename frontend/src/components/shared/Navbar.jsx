35d969fe4313cc71819e4952f7d8bbfd6eef3463
Mr zakir ullah <mrzak802@gmail.com>
2024-12-29 05:22:47 -0800

    comment

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,18 +1,20 @@
 import { Popover, PopoverTrigger } from "@radix-ui/react-popover";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import React from "react";
 import { PopoverContent } from "../ui/popover";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { LogOut, User2 } from "lucide-react";
 
 function Navbar() {
   return (
     <div className="bg-white">
-      <div className="flex items-center justify-between mx-w-7xl h-16">
+      <div className="flex items-center justify-between mx-auto mx-w-7xl h-16 ">
         <div>
           <h1 className="text-2xl font-bold">
             Job <span className="text-red-500">Portal</span>
           </h1>
         </div>
-        <div>
+        <div className="flex items-center gap-12">
           <ul className="flex font-medium item-center gap-5">
             <li>Home</li>
             <li>Job</li>
@@ -20,10 +22,40 @@ function Navbar() {
           </ul>
           <Popover>
             <PopoverTrigger asChild>
-              <Button variant="outline">Open popover</Button>
+              <Avatar className="cursor-pointer w-12 h-12 rounded-full">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                  className="w-full h-full rounded-full"
+                />
+              </Avatar>
             </PopoverTrigger>
-            <PopoverContent>
-                <h1>hello </h1>
+            <PopoverContent className="w-80">
+              <div className="flex gap-4 space-y-2">
+                <Avatar className="cursor-pointer w-12 h-12 rounded-full">
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                    className="w-full h-full rounded-full"
+                  />
+                </Avatar>
+                <div>
+                  <h4 className="font-medium">zakir khan</h4>
+                  <p className="text-sm text-muted-foreground">
+                    Mern stack developer
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col text-grey-600">
+                <div className="flex w-fit items-center gap-3 cursor-pointer">
+                  <User2 />
+                  <Button  variant="link" className="border-none">View Profile</Button>
+                </div>
+                <div className="flex w-fit items-center gap-3 cursor-pointer">
+                  <LogOut />
+                  <Button variant="link" className="border-none">Logout</Button>
+                </div>
+              </div>
             </PopoverContent>
           </Popover>
         </div>
