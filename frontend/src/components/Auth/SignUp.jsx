e22436f2b02005a538f581692e7006ef65eb945e
Mr zakir ullah <mrzak802@gmail.com>
2025-01-04 11:25:02 -0800

    comment

--- frontend/src/components/Auth/SignUp.jsx
+++ frontend/src/components/Auth/SignUp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../shared/Navbar";
 import { Label } from "../ui/label";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 
 function SignUp() {
   return (
@@ -43,6 +44,19 @@ function SignUp() {
               type="password"
               className="w-full p-2 border rounded"
             />
+          </div>
+          <div>
+            <RadioGroup defaultValue="comfortable">
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="default" id="r1" />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="comfortable" id="r2" />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            
+            </RadioGroup>
           </div>
         </form>
       </div>
