3a9b740381c3f8127596e2686d3fa745efdabd9f
Mr zakir ullah <mrzak802@gmail.com>
2025-01-11 13:38:32 -0800

    comment

--- frontend/src/components/AppliedJobTable/AppliedJobTable.jsx
+++ frontend/src/components/AppliedJobTable/AppliedJobTable.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
+import { Badge } from '../ui/badge'
 
 function AppliedJobTable() {
+    const dumeArry = [1,2,3,4]
   return (
-    <div>AppliedJobTable</div>
+    <div>
+    <Table>
+        <TableCaption>A list of your applied jobs</TableCaption>
+        <TableHeader>
+            <TableRow>
+                <TableHead>Date</TableHead>
+                <TableHead>Job Role</TableHead>
+                <TableHead>Company</TableHead>
+                <TableHead className="text-right">Status</TableHead>
+            </TableRow>
+        </TableHeader>
+        <TableBody>
+            {
+                dumeArry.length <= 0 ? <span>You haven't applied any job yet.</span> : dumeArry.map((item,index) => (
+                    <TableRow key={index}>
+                        <TableCell>12/01/24</TableCell>
+                        <TableCell>backend developer</TableCell>
+                        <TableCell>Google</TableCell>
+                        <TableCell className="text-right"><Badge>Selected</Badge></TableCell>
+                    </TableRow>
+                ))
+            }
+        </TableBody>
+    </Table>
+</div>
   )
 }
 
